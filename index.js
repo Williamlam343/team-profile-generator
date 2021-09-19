fb7bec9fd47a168add0a40b06f3cf3039148674b
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-09-19 13:05:49 -0400

    fully intergated question prompts to create employee classes

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 console.clear()
-const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
 const inquirer = require("inquirer")
-
-
+const buildCard = require("./lib/generatehtml")
+const fs = require('fs');
+let employeeList = []
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 const managerQuestions = [
     {
         type: 'input',
-        name: 'managerName',
-        message: 'Enter team manager name',
+        name: 'name',
+        message: 'Enter team manager name:',
     },
     {
         type: 'number',
-        name: 'ID',
-        message: 'Enter employee ID',
+        name: 'id',
+        message: 'Enter employee ID:',
         validate(value) {
             if (Number.isInteger(value)) {
                 return true;
@@ -25,12 +27,12 @@ const managerQuestions = [
     {
         type: 'input',
         name: 'email',
-        message: 'Enter email address',
+        message: 'Enter email address:',
     },
     {
         type: 'number',
         name: 'officeNumber',
-        message: 'Enter office number',
+        message: 'Enter office number:',
         validate(value) {
             if (Number.isInteger(value)) {
                 return true;
@@ -38,6 +40,52 @@ const managerQuestions = [
             return 'Please enter a number';
         },
     }
+]
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter intern name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter intern ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter intern email:',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter intern school:',
+    },
+
+]
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter engineer name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter engineer ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter engineer email:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter engineer github:',
+    },
+
 ]
 
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
@@ -55,36 +103,65 @@ function buildTeam(choice) {
         // WHEN I select the engineer option
         // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
         case "Add an Engineer":
-            inquirer.prompt(engineerQuestions)
+            inquirer.prompt(engineerQuestions).then((res) => {
+                // create a new Engineer obj 
+                let employee = new Engineer(res.name, res.id, res.email, res.github)
+                // add into employee [{}]
+                employeeList.push(employee)
+                console.log(employeeList)
+                inquirer.prompt(buildTeamQuestions)
+                    .then((choice) => {
+                        buildTeam(choice.menu)
+                    })
+            }
+            )
             break;
         // WHEN I select the intern option
         // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
         case "Add an Intern":
-            inquirer.prompt(internQuestions)
+            inquirer.prompt(internQuestions).then((res) => {
+                // create a new Intern obj
+                let employee = new Intern(res.name, res.id, res.email, res.school)
+                // add into employee [{}]
+                employeeList.push(employee)
+                console.log(employeeList)
+                // brings manager back to menu
+                inquirer.prompt(buildTeamQuestions).then((choice) => {
+                    buildTeam(choice.menu)
+                })
+            })
             break;
 
         default:
+            buildTeam(employeeList)
             // WHEN I decide to finish building my team
             // THEN I exit the application, and the HTML is generated
 
-            break;
+            break; cd
     }
 }
+
+
 
 function init() {
     inquirer
         .prompt(managerQuestions)
         .then((resp) => {
-            console.log(resp, `Welcome ${resp.managerName}!`),
-                inquirer.prompt(buildTeamQuestions).then((choice) => {
-                    console.log(choice);
-                    // buildTeam(choice.menu)
-                })
+            console.log(resp, `Welcome ${resp.name}!\n`);
+            // create a new Manager obj 
+            let employee = new Manager(resp.name, resp.id, resp.email, resp.officeNumber)
+            // add into employee [{}]
+            employeeList.push(employee)
+            console.log(employeeList)
+            inquirer.prompt(buildTeamQuestions).then((choice) => {
+                buildTeam(choice.menu)
+            })
 
         })
 
 }
 
 init()
-// const tucker = new Manager("Tucker", 1, "@gmail", 101)
+
+// const tucker = new Intern("Tucker", 1, "@gmail", 101)
 // console.log(tucker.getRole())
